261dc29ba5afd600a11186b43b2c620aef09b292
Anton Liauchuk <anton93lev@gmail.com>
2019-10-13 01:08:04 +0300

    dependencies, basic entities

---
+++ src/main/java/com/user/management/model/Document.java
@@ -0,0 +1,4 @@
+package com.user.management.model;
+
+public class Document {
+}
---
+++ src/main/java/com/user/management/model/Role.java
@@ -0,0 +1,12 @@
+package com.user.management.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_ADMIN, ROLE_CLIENT;
+
+    public String getAuthority() {
+        return name();
+    }
+
+}
