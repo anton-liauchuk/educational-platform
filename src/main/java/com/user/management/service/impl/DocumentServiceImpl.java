f9982657e9d2e51bfa6783c59de4210dd2667acb
Anton Liauchuk <anton93lev@gmail.com>
2019-10-20 08:31:03 +0300

    custom resource not found exception

--- src/main/java/com/user/management/service/impl/DocumentServiceImpl.java
+++ src/main/java/com/user/management/service/impl/DocumentServiceImpl.java
@@ -1,11 +1,15 @@
 package com.user.management.service.impl;
 
+import com.user.management.exception.ResourceNotFoundException;
 import com.user.management.model.Document;
 import com.user.management.repository.DocumentRepository;
 import com.user.management.service.DocumentService;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DocumentServiceImpl implements DocumentService {
@@ -24,12 +28,25 @@ public class DocumentServiceImpl implements DocumentService {
 
     @Override
     public void deleteById(Integer id) {
-        documentRepository.deleteById(id);
+        try {
+            documentRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(e.getMessage());
+        }
     }
 
     @Override
     public Page<Document> findAll(Pageable pageable) {
         return documentRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Document> findById(Integer id) {
+        final Optional<Document> document = documentRepository.findById(id);
+        if (document.isEmpty()) {
+            throw new ResourceNotFoundException("No document available with id = " + id);
+        }
+        return document;
     }
 }
 
