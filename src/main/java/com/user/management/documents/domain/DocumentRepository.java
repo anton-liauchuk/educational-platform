de63fcdb2b840a148b47a081f783aaac0ca758ee
Anton Liauchuk <anton93lev@gmail.com>
2020-01-21 07:18:30 +0300

    - parent gradle project settings; - user service module; - remove not actual changes;

--- src/main/java/com/user/management/documents/domain/DocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.user.management.documents.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentRepository extends JpaRepository<Document, Integer> {
-}
-
--- src/main/java/com/user/management/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.user.management.repository;
-
-import com.user.management.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
-
