937c6f4dccbde17c2501ae936b87a6e418d427ef
Anton Liauchuk <anton93lev@gmail.com>
2020-01-26 08:44:11 +0300

    - configuration module with spring boot dependency/application

---
+++ configuration/src/main/java/com/educational/platform/configuration/EducationalPlatformApplication.java
@@ -0,0 +1,14 @@
+package com.educational.platform.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalPlatformApplication.class, args);
+    }
+
+}
+
