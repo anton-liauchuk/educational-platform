c09e67ed656fac79e4fc3ee122b998a07595d238
Anton Liauchuk <anton93lev@gmail.com>
2020-08-12 19:49:01 +0300

    #1 Query handlers and corresponding endpoints: - list course query implementation;

---
+++ courses/application/src/main/java/com/educational/platform/courses/course/query/ListCourseQueryHandler.java
@@ -0,0 +1,35 @@
+package com.educational.platform.courses.course.query;
+
+import java.util.List;
+
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import com.educational.platform.courses.course.CourseDTO;
+import com.educational.platform.courses.course.CourseRepository;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Query handler for getting the list of courses.
+ */
+@RequiredArgsConstructor
+@Component
+public class ListCourseQueryHandler {
+
+	private final CourseRepository repository;
+
+	/**
+	 * Retrieves list of course dtos.
+	 *
+	 * @param query query.
+	 * @return corresponding list of course dtos.
+	 */
+	@QueryHandler
+	@NonNull
+	public List<CourseDTO> handle(ListCourseQuery query) {
+		return repository.list();
+	}
+
+}
