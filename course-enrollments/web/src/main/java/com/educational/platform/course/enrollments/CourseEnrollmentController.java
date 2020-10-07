1bf57cbff29480e45737ee5115e6d61e0cc3d7d4
Anton Liauchuk <anton93lev@gmail.com>
2020-10-07 22:25:24 +0300

    #1 Query handlers and corresponding endpoints: - list course enrollments;

--- course-enrollments/web/src/main/java/com/educational/platform/course/enrollments/CourseEnrollmentController.java
+++ course-enrollments/web/src/main/java/com/educational/platform/course/enrollments/CourseEnrollmentController.java
@@ -1,12 +1,16 @@
 package com.educational.platform.course.enrollments;
 
+import com.educational.platform.course.enrollments.query.ListCourseEnrollmentsQuery;
 import com.educational.platform.course.enrollments.register.RegisterStudentToCourseCommand;
 import lombok.RequiredArgsConstructor;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -19,6 +23,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CourseEnrollmentController {
 
     private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
 
     @PostMapping(value = "/courses/{uuid}/course-enrollments", produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
@@ -28,5 +33,11 @@ public class CourseEnrollmentController {
                 .build();
 
         return commandGateway.sendAndWait(command);
+    }
+
+    @GetMapping(value = "/course-enrollments", produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<CourseEnrollmentDTO> courseEnrollments() {
+        return queryGateway.query(new ListCourseEnrollmentsQuery(), ResponseTypes.multipleInstancesOf(CourseEnrollmentDTO.class)).join();
     }
 }
