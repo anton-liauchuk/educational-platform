c488f78eaca7c6af8712ac6c965f2133663c9747
Anton Liauchuk <anton93lev@gmail.com>
2025-09-15 15:37:34 +0300

    whitelist api-docs url

--- users/web/src/main/java/com/educational/platform/users/security/WebSecurityConfig.java
+++ users/web/src/main/java/com/educational/platform/users/security/WebSecurityConfig.java
@@ -30,8 +30,8 @@ public class WebSecurityConfig {
 		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(
 				"/users/sign-in",
 				"/users/sign-up",
-				"/v2/api-docs",
-				"/v3/api-docs",
+				"/v2/api-docs/**",
+				"/v3/api-docs/**",
 				"/swagger-resources/**",
 				"/swagger-ui.html",
 				"/swagger-ui/**",
