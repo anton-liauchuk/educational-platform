9baaf0e38d2c983790a9a2ecb4b9646063947097
Anton Liauchuk <anton93lev@gmail.com>
2026-03-15 15:11:01 +0300

    enable tests

--- courses/application/src/test/java/com/educational/platform/courses/course/query/ListCourseQueryHandlerIntegrationTest.java
+++ courses/application/src/test/java/com/educational/platform/courses/course/query/ListCourseQueryHandlerIntegrationTest.java
@@ -2,11 +2,7 @@ package com.educational.platform.courses.course.query;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
@@ -15,14 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.educational.platform.courses.course.Course;
 import com.educational.platform.courses.course.CourseRepository;
 import com.educational.platform.courses.course.create.CreateCourseCommand;
-import com.educational.platform.courses.course.create.CreateLectureCommand;
-import com.educational.platform.courses.course.create.CreateQuestionCommand;
-import com.educational.platform.courses.course.create.CreateQuizCommand;
 import com.educational.platform.courses.teacher.Teacher;
 import com.educational.platform.courses.teacher.TeacherRepository;
 import com.educational.platform.courses.teacher.create.CreateTeacherCommand;
 
-@Disabled
 @AutoConfigureTestDatabase
 @SpringBootTest
 public class ListCourseQueryHandlerIntegrationTest {
@@ -41,15 +33,7 @@ public class ListCourseQueryHandlerIntegrationTest {
 		var teacher = new Teacher(new CreateTeacherCommand("teacher"));
 		teacherRepository.save(teacher);
 
-		var course = new Course(CreateCourseCommand.builder().name("name").description("description")
-				.curriculumItems(Arrays.asList(
-						CreateLectureCommand.builder().title("lecture title").text("text").build(),
-						CreateQuizCommand
-								.builder()
-								.title("quiz title")
-								.questions(Collections.singletonList(new CreateQuestionCommand("question text")))
-								.build()
-				)).build(), teacher.getId());
+		var course = new Course(CreateCourseCommand.builder().name("name").description("description").build(), teacher.getId());
 		courseRepository.save(course);
 	}
 
--- users/web/src/test/java/com/educational/platform/users/UserControllerIntegrationTest.java
+++ users/web/src/test/java/com/educational/platform/users/UserControllerIntegrationTest.java
@@ -5,7 +5,6 @@ import com.educational.platform.users.security.WebSecurityConfig;
 
 import org.axonframework.extension.springboot.autoconfig.SecurityAutoConfiguration;
 import org.axonframework.messaging.commandhandling.gateway.CommandGateway;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -21,7 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Represents course controller integration tests.
  */
-@Disabled
 @WebMvcTest(value = UserController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class}, excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = WebSecurityConfig.class)})
 public class UserControllerIntegrationTest {
